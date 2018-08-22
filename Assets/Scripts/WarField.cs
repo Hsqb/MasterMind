7145ab8d7facd627982758afaea5077d10c5dc6d
hhan <hhan87@gmail.com>
2018-08-22 15:00:10 +0900

    fixed for ethereum

--- Assets/Scripts/WarField.cs
+++ Assets/Scripts/WarField.cs
@@ -15,7 +15,7 @@ public class WarField {
 
 
 	public WarField(){
-		turn = 1;
+		turn = 2;
         masterMind = new EnemyAi(2000, 10, 0);
         warMachine = new OurAi(500, 10, 0);
     }
@@ -37,27 +37,28 @@ public class WarField {
         string[] tmpArr = new string[]{
             version.ToString(),
             turn.ToString(),
-            warMachine.GetHp().ToString(),
+			warMachine.GetAP().GetCurrentAp().ToString(),
+            warMachine.GetAP().GetCurrentMaxAp().ToString(),
             warMachine.GetArmy().GetArmyNum().ToString(),
             warMachine.GetArmy().GetAdvArmyNum().ToString(),
-            warMachine.GetAP().GetCurrentAp().ToString(),
-            warMachine.GetAP().GetCurrentMaxAp().ToString(),
+			masterMind.GetArmy().GetArmyNum().ToString(),
+			masterMind.GetArmy().GetAdvArmyNum().ToString(),
             warMachine.GetResourceManager().ToString(),
             String.Join("",warMachine.GetFaclilties().Select( x => x.CheckIsActivated()?"1":"0" ).ToArray()),
-            warMachine.GetHp().ToString(),
-            warMachine.GetArmy().GetArmyNum().ToString(),
-            warMachine.GetArmy().GetAdvArmyNum().ToString()
+			warMachine.GetHp().ToString(),
+			masterMind.GetHp().ToString(),
+
         };
         return String.Join(":", tmpArr);
     }
     public void Parse(string savedData)
     {
-        
+
         string[] data = savedData.Split(':');
         //version Check
         if (data[0] == "1")
         {
-            
+
             //turn
             this.turn = Int32.Parse(data[1]);
             //ourAiHp
@@ -69,9 +70,9 @@ public class WarField {
             int savedMaxAp = Int32.Parse(data[6]);
             for (int i = 0; i < AP.MAX_AP; i++)
             {
-                if(this.warMachine.GetAP().GetCurrentMaxAp() < savedMaxAp) { 
+                if(this.warMachine.GetAP().GetCurrentMaxAp() < savedMaxAp) {
                     this.warMachine.GetAP().AddCurrentMaxAp();
-                    
+
                 }
             }
             this.warMachine.GetAP().ResetCurrentAp();
